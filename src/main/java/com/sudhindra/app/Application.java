9844f583871c2902b3bdec67006d60bce7fac721
sudhindrasaxena <sudhindra.saxena@hotmail.com>
2018-11-10 20:03:28 +0530

    name changes

--- src/main/java/com/sudhindra/app/Application.java
+++ src/main/java/com/sudhindra/app/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HibernateSpringbootApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HibernateSpringbootApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
